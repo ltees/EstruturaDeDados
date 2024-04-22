aea2e1e45967b6902ed2b0a33fa87c9e8fb8f160
ltees <135617476+ltees@users.noreply.github.com>
2024-04-22 18:02:12 -0300

    Atv1.c

--- Atv1.c
+++ Atv1.c
@@ -1,20 +1,100 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+#define MAX_NOME 50
+#define MAX_TELEFONE 15
+#define MAX_CURSO 50
+#define MAX_NOTAS 5
+#define MAX_ALUNOS 100
 
-    float nota;
-    char nome[60] = "";
+typedef struct {
+    char nome[MAX_NOME];
+    char telefone[MAX_TELEFONE];
+    char curso[MAX_CURSO];
+    float notas[MAX_NOTAS];
+    float media;
+    char situacao[10];
+} Aluno;
 
-    printf("Insira seu primeiro nome:\n");
-    scanf("%s", &nome);
+int lerDados(Aluno alunos[]) {
+    FILE *entrada;
+    char nome_arquivo_entrada[] = "DadosEntrada.csv";
+    int num_alunos = 0;
 
-    printf("Insira sua nota:\n");
-    scanf("%f", &nota);
-
-    if(nota >= 7.0){
-        printf("Aprovado(a)\n");
-    } else{
-        printf("Reprovado(a)\n");
+    entrada = fopen(nome_arquivo_entrada, "r");
+    if (entrada == NULL) {
+        printf("Erro ao abrir o arquivo de entrada.\n");
+        return -1;
     }
 
+    while (fscanf(entrada, "%[^,],%[^,],%[^,],%f,%f\n", alunos[num_alunos].nome,
+                  alunos[num_alunos].telefone, alunos[num_alunos].curso,
+                  &alunos[num_alunos].notas[0], &alunos[num_alunos].notas[1]) != EOF) {
+        num_alunos++;
+    }
+
+    fclose(entrada);
+    return num_alunos;
+}
+
+void calcularSituacao(Aluno alunos[], int num_alunos) {
+    for (int i = 0; i < num_alunos; i++) {
+        alunos[i].media = (alunos[i].notas[0] + alunos[i].notas[1]) / 2;
+        if (alunos[i].media >= 7.0)
+            strcpy(alunos[i].situacao, "APROVADO");
+        else
+            strcpy(alunos[i].situacao, "REPROVADO");
+    }
+}
+
+void escreverSituacao(Aluno alunos[], int num_alunos) {
+    FILE *saida;
+    char nome_arquivo_saida[] = "SituacaoFinal.csv";
+
+    saida = fopen(nome_arquivo_saida, "w");
+    if (saida == NULL) {
+        printf("Erro ao abrir o arquivo de saida.\n");
+        return;
+    }
+
+    for (int i = 0; i < num_alunos; i++) {
+        fprintf(saida, "%s, %.2f, %s\n", alunos[i].nome, alunos[i].media, alunos[i].situacao);
+    }
+
+    fclose(saida);
+    printf("Arquivo %s gerado com sucesso.\n", nome_arquivo_saida);
+}
+
+int main() {
+    Aluno alunos[MAX_ALUNOS];
+    int num_alunos;
+
+    int opcao;
+    do {
+        printf("\nMenu:\n");
+        printf("1. Processar dados dos alunos\n");
+        printf("2. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                num_alunos = lerDados(alunos);
+                if (num_alunos <= 0) {
+                    printf("Nenhum aluno encontrado.\n");
+                    break;
+                }
+                calcularSituacao(alunos, num_alunos);
+                escreverSituacao(alunos, num_alunos);
+                break;
+            case 2:
+                printf("Encerrando o programa.\n");
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+    } while (opcao != 2);
+
+    return 0;
 }
